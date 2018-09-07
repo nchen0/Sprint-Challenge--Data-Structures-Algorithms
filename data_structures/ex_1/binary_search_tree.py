09ca1c2408656c9c53afa180caab7f528fa21c97
Nicky Chen <nicky.chen0@gmail.com>
2018-09-07 09:12:35 -0700

    task 1 completed

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,9 +5,23 @@ class BinarySearchTree:
         self.right = None
 
     def depth_first_for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.depth_first_for_each(cb)
+        if self.right:
+            self.right.depth_first_for_each(cb)
 
     def breadth_first_for_each(self, cb):
-        pass
+        queue = []
+        if self.left:
+            queue.insert(0, self.left)
+        if self.right:
+            queue.insert(0, self.right)
+        if not queue:
+            return None
+        node = queue.pop()
+        if node:
+            node.breadth_first_for_each(cb, queue)
 
     def insert(self, value):
         new_tree = BinarySearchTree(value)
